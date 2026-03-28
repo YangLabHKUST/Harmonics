6e9a5a43ecc2bc2ec454db071800a0ea92429c94
Yuyao Liu <2645751574@qq.com>
2026-03-28 22:46:52 +0800

    v1.0.1

--- Harmonics/__init__.py
+++ Harmonics/__init__.py
@@ -9,7 +9,7 @@ __author__ = "Yuyao Liu"
 __email__ = "yliuow@connect.ust.hk"
 
 from .model import Harmonics_Model
-from .hypo_test import ct_enrichment_test, cci_enrichment_test, nnc_enrichment_test
+from .hypo_test import ct_enrichment_test, cci_enrichment_test, nnc_enrichment_test, cal_nnc_mtx, nnc_between_groups_test
 from .utils import Delaunay_adjacency_mtx, knn_adjacency_matrix, joint_adjacency_matrix, \
     index2onehot, label2onehot, label2onehot_anndata, calculate_distribution, update_microenvironment, \
     pca, measure_distribution_gap, cell2cellniche, cell2cellniche_cond, ctr_cond_merge, refine_dist
